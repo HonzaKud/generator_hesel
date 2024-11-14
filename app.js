3f8b7e0a9b56b4d0ae799d4c6cfce46dd32d562e
Jan Kudrna <jan.kudrna.tt@gmail.com>
2024-11-14 09:04:36 +0100

    vytvoreni vsech funkci pro inputy od uzivatele

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ function askNumberOfPasswords(callback) {
 
 // Funkce na otazku na uzivetele na pocet znaku ve vygenerovanym hesle
 function askNumberOfCharacters(callback) {
-    rl.question('Kolik znaku v hesle chces vygenerovat? (Zadejte číslo od 1 do 9): ', (answer) => { // readline otazka na pocet znaku v hesle
+    rl.question('Kolik znaku v hesle chces vygenerovat? (Zadejte číslo od 8 do 30): ', (answer) => { // readline otazka na pocet znaku v hesle
         let numberOfCharacters = parseInt(answer); // ulozeni odpovedi do promenne numberOfCharacters a prevedeni na cislo
         
-        if (isNaN(numberOfCharacters) || numberOfCharacters < 1 || numberOfCharacters > 9) { // kontrola jestli je odpoved uzivatele cislo a jestli je mezi 1 az 9
-            console.log("Zadejte prosim platne cislo od 1 do 9."); //pokud odpoved neni platna, console vypise tohle
+        if (isNaN(numberOfCharacters) || numberOfCharacters < 8 || numberOfCharacters > 30) { // kontrola jestli je odpoved uzivatele cislo a jestli je mezi 8 az 30
+            console.log("Zadejte prosim platne cislo od 8 do 30."); //pokud odpoved neni platna, console vypise tohle
             askNumberOfCharacters(callback); // znovu zavola funkci s callbackem, pokud neni platny vztup
         } else {
             console.log(`Pocet znaku v kazdem heslo bude: ${numberOfCharacters}`); // vypise pocet znaku
@@ -47,22 +47,66 @@ function askNumberOfCharacters(callback) {
 }
 
 // funkce pro otazku pro uzivatele, jestli chce pouzit ve vygenerovanych heslech velka pismena
-function askUseUppercase() {
+function askUseUppercase(callback) {
     rl.question('Chcete, aby hesla obsahovala velka pismena? (ano/ne): ', (answer) => { // readline otazka jestli chce uzivatel v hesle velka pismena
         const normalizedAnswer = answer.toLowerCase(); // do promenne normalizedAnswer se ulozi input od uzivatele ale jeste se prevede na mala pismenena, pokud jsou velka
         
         if (normalizedAnswer === 'ano') {
             console.log('Velka pismena budou zahrnuta do hesel.');
-            rl.close(); // ukonceni vstupu, pokud je odpoved platna
+            callback();
         } else if (normalizedAnswer === 'ne') {
             console.log('Velka pismena nebudou zahrnuta do hesel.');
-            rl.close(); // ukonceni vstupu, pokud je odpoved platna
+           callback();
         } else {
             console.log("Zadejte prosim pouze 'ano' nebo 'ne'.");
-            askUseUppercase(); // Znovu zavola funkci, pokud vstup neni platny
+            askUseUppercase(callback); // Znovu zavola funkci, pokud vstup neni platny
         }
     });
 }
 
-// Zavolani prvni funkce s callbackem pro dalsi a dalsi otazku
-askNumberOfPasswords(() => askNumberOfCharacters(askUseUppercase));
+// funkce pro otazku na uzivatele, jestli chce pouzit ve vygenerovanych heslech cisla
+function askUseNumber(callback) {
+    rl.question('Chcete, aby hesla obsahovala cisla? (ano/ne): ', (answer) => { // readline otazka jestli chce uzivatel v hesle cisla
+        const normalizedAnswer = answer.toLowerCase(); // do promenne normalizedAnswer se ulozi input od uzivatele ale jeste se prevede na mala pismenena, pokud jsou velka
+        
+        if (normalizedAnswer === 'ano') {
+            console.log('Cisla budou zahrnuta do hesel.');
+            callback()
+        } else if (normalizedAnswer === 'ne') {
+            console.log('Cisla nebudou zahrnuta do hesel.');
+            callback()
+        } else {
+            console.log("Zadejte prosim pouze 'ano' nebo 'ne'.");
+            askUseNumber(callback); // Znovu zavola funkci, pokud vstup neni platny
+        }
+    });
+}
+
+// funkce pro otazku na uzivatele, jestli chce pouzit ve vygenerovanych heslech specialni znaky
+function askUseSpecialCharacters() {
+    rl.question('Chcete, aby hesla obsahovala specialni znaky? (ano/ne): ', (answer) => { // readline otazka jestli chce uzivatel v hesle specialni znaky
+        const normalizedAnswer = answer.toLowerCase(); // do promenne normalizedAnswer se ulozi input od uzivatele ale jeste se prevede na mala pismenena, pokud jsou velka
+        
+        if (normalizedAnswer === 'ano') {
+            console.log('Specialni znaky budou zahrnuta do hesel.');
+            rl.close(); // ukonceni vstupu, pokud je odpoved platna
+        } else if (normalizedAnswer === 'ne') {
+            console.log('Specialni znaky nebudou zahrnuta do hesel.');
+            rl.close(); // ukonceni vstupu, pokud je odpoved platna
+        } else {
+            console.log("Zadejte prosim pouze 'ano' nebo 'ne'.");
+            askUseSpecialCharacters(); // Znovu zavola funkci, pokud vstup neni platny
+        }
+    });
+}
+
+// Zavolani funkci ve spravnem poradi
+askNumberOfPasswords(() => 
+    askNumberOfCharacters(() => 
+        askUseUppercase(() => 
+            askUseNumber(() =>
+                askUseSpecialCharacters()   
+            )
+        )
+    )
+);
